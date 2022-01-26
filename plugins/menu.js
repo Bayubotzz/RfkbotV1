8a782d2fe9e56928bd1d4343d60383c71f986c47
Bayubotzz <96980508+Bayubotzz@users.noreply.github.com>
2022-01-26 23:43:09 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -238,12 +238,12 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
 ┃⬡ *${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length}* Chat Terbanned
 ┃⬡ *${Object.entries(global.db.data.users).filter(user => user[1].banned).length}* Pengguna Terbanned
 ┃
-┃⬡ Script by Alyaa
-┃⬡ Yt : AlyaaXzy
+┃⬡ Script by BAYU OFC
+┃⬡ Yt : BAYU OFC
 ┃⬡ Run bot : Heroku
-┃⬡ Tipe SC rfkbot
+┃⬡ Tipe SC ELAINA-BOT
 ┗━━━━━━━━⬣`.trim(),
-          "buttonText": "Klik Disini",
+          "buttonText": "DAFTAR MENU",
           "listType": "SINGLE_SELECT",
           "sections": [
             {
