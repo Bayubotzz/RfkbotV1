2870638fa6493491c263da9e2829af01e95b47ee
Bayubotzz <96980508+Bayubotzz@users.noreply.github.com>
2022-01-26 23:51:28 +0700

    Update viadana.js

--- plugins/viadana.js
+++ plugins/viadana.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
 	conn.reply(m.chat, `╠═〘 PEMBAYARAN 〙 ═
 ╠═ Untuk Pembayaran Via DANA
 ╠➥ Ke nomer ini
-║➥ 089525518325
+║➥ 081285866502
 ║
 ╠═〘 Fitur 〙 ═
 ║
@@ -22,8 +22,8 @@ let handler = async (m, { conn }) => {
 ╠➥ *DAN 200 LEBIH*
 ║   *FITUR LAINNYA*
 ║ 
-╠═ ©2021 Rpg wabot-aq
-╠═ Scrip original by Nurutomo
+╠═ ©2021 Rpg elaina-bot
+╠═ Scrip original by Bayu Ofc
 ╠═〘 ${namabot} 〙 ═`.trim(), m)
 }
 
